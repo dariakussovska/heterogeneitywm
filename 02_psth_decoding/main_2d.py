9b061cd22a62e0fce23d0e2c117817001fd1f0bc
Daria Kussovska <159807265+dariakussovska@users.noreply.github.com>
2025-10-14 15:32:54 -0400

    Update main_2d.py

--- 02_psth_decoding/main_2d.py
+++ 02_psth_decoding/main_2d.py
@@ -8,7 +8,7 @@ import os
 from matplotlib.colors import LinearSegmentedColormap
 from scipy.ndimage import label, find_objects
 
-trial_info = pd.read_excel('/home/daria/PROJECT/new_trial_final.xlsx')
+trial_info = pd.read_feather('/home/daria/PROJECT/new_trial_final.feather')
 subject_trials = trial_info[trial_info['subject_id'] == 14][['trial_id_final', 'num_images_presented', 'stimulus_index_enc1', 'stimulus_index_enc2', 'stimulus_index_enc3', 'response_accuracy']]
 print(subject_trials)
 
@@ -17,12 +17,12 @@ y_matrix = subject_trials
 # ===============================
 # 1. Load Spike Data from All Periods
 # ===============================
-df_enc1 = pd.read_excel('/home/daria/PROJECT/clean_data/cleaned_Encoding1.xlsx')
-df_enc2 = pd.read_excel('/home/daria/PROJECT/clean_data/cleaned_Encoding2.xlsx')
-df_enc3 = pd.read_excel('/home/daria/PROJECT/clean_data/cleaned_Encoding3.xlsx')
-df_delay = pd.read_excel('/home/daria/PROJECT/clean_data/cleaned_Delay.xlsx')
-df_probe = pd.read_excel('/home/daria/PROJECT/clean_data/cleaned_Probe.xlsx')
-df_fixation = pd.read_excel('/home/daria/PROJECT/clean_data/cleaned_Fixation.xlsx')
+df_enc1 = pd.read_feather('/home/daria/PROJECT/clean_data/cleaned_Encoding1.feather')
+df_enc2 = pd.read_feather('/home/daria/PROJECT/clean_data/cleaned_Encoding2.feather')
+df_enc3 = pd.read_feather('/home/daria/PROJECT/clean_data/cleaned_Encoding3.feather')
+df_delay = pd.read_feather('/home/daria/PROJECT/clean_data/cleaned_Delay.feather')
+df_probe = pd.read_feather('/home/daria/PROJECT/clean_data/cleaned_Probe.feather')
+df_fixation = pd.read_feather('/home/daria/PROJECT/clean_data/cleaned_Fixation.feather')
 
 y_matrix = y_matrix.reset_index(drop=True)
 neuron_ids = df_enc1[df_enc1['Signi'] == 'Y']['Neuron_ID_3'].unique()
