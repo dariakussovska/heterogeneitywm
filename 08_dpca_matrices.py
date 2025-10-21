06a726ffa5ffc741b42172a0de9ff5818f5346a9
Daria Kussovska <159807265+dariakussovska@users.noreply.github.com>
2025-10-21 14:37:13 -0400

    Change file paths to relative in 08_dpca_matrices.py
    Updated file paths to use relative paths instead of absolute paths.

--- 08_dpca_matrices.py
+++ 08_dpca_matrices.py
@@ -11,7 +11,7 @@ from scipy.spatial.distance import pdist
 import scipy.stats as sps
 import scikit_posthocs as sp
 
-trial_info = pd.read_excel('/home/daria/PROJECT/new_trial_info.xlsx')
+trial_info = pd.read_excel('/./new_trial_info.xlsx')
 subject_trials = trial_info[(trial_info['subject_id'] == 14) & (trial_info['num_images_presented'] == 1)][['new_trial_id', 'num_images_presented', 'stimulus_index']]
 y_matrix = subject_trials
 
@@ -33,9 +33,9 @@ print(balanced_distribution)
 
 balanced_y_matrix = y_matrix_balanced
 
-fixation_data_with_brain_region = pd.read_excel('/home/daria/PROJECT/graph_data/graph_fixation.xlsx')
-enc1_data_with_brain_region = pd.read_excel('/home/daria/PROJECT/clean_data/cleaned_Encoding1.xlsx')
-delay_data_with_brain_region = pd.read_excel('/home/daria/PROJECT/graph_data/graph_delay.xlsx')
+fixation_data_with_brain_region = pd.read_excel('/./graph_data/graph_fixation.xlsx')
+enc1_data_with_brain_region = pd.read_excel('/./clean_data/cleaned_Encoding1.xlsx')
+delay_data_with_brain_region = pd.read_excel('/./graph_data/graph_delay.xlsx')
 
 fixation_data = fixation_data_with_brain_region
 
@@ -237,7 +237,7 @@ print(trialD.shape)
 import numpy as np
 
 # Define your directory path
-save_directory = "/home/daria/PROJECT/"  # Replace with your desired path
+save_directory = "/./"  # Replace with your desired path
 
 # Save individual files
 np.save(f"{save_directory}trialE.npy", trialE)
