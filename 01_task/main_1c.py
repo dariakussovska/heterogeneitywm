9421d00aef2d4541e1d44ab8df187b6f91c15225
Daria Kussovska <159807265+dariakussovska@users.noreply.github.com>
2025-10-21 19:37:23 -0400

    Update main_1c.py

--- 01_task/main_1c.py
+++ 01_task/main_1c.py
@@ -5,8 +5,8 @@ import seaborn as sns
 from scipy.stats import kruskal, mannwhitneyu
 import itertools
 
-spike_data_path = "./all_spike_rate_data_probe.feather"
-trial_info_path = "./trial_info.feather"
+spike_data_path = "../all_spike_rate_data_probe.feather"
+trial_info_path = "../trial_info.feather"
 
 df = pd.read_feather(spike_data_path)
 trial_info = pd.read_feather(trial_info_path)
@@ -83,5 +83,5 @@ for i, r in enumerate(results):
     add_sig_bracket(ax, x1, x2, y, h=0.02 + 0.1*step, stars=r['stars'])
 
 plt.tight_layout()
-plt.savefig("./01_task/main_1c.eps", format="eps", dpi=300)
+plt.savefig("/01_task/main_1c.eps", format="eps", dpi=300)
 plt.show()
