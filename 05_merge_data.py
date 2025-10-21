91d1595406df350fcf52b4712a3a72ea1db4b588
Daria Kussovska <159807265+dariakussovska@users.noreply.github.com>
2025-10-21 17:31:33 -0400

    Update 05_merge_data.py

--- 05_merge_data.py
+++ 05_merge_data.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import os
 
-BASE_DIR = '/./'
+BASE_DIR = './'
 CLEAN_DATA_DIR = os.path.join(BASE_DIR, 'clean_data')
 GRAPH_DATA_DIR = os.path.join(BASE_DIR, 'graph_data')
 
 # Load all source data
-significant_neurons_df = pd.read_feather('/./Neuron_Check_Significant_All.feather')
-trial_info_df = pd.read_feather('/./trial_info.feather')
-brain_regions_df = pd.read_feather('/./data/all_neuron_brain_regions_cleaned.feather')
+significant_neurons_df = pd.read_feather('./Neuron_Check_Significant_All.feather')
+trial_info_df = pd.read_feather('./trial_info.feather')
+brain_regions_df = pd.read_feather('./data/all_neuron_brain_regions_cleaned.feather')
 
 # Filter significant neurons to only include Y or N
 significant_neurons_filtered = significant_neurons_df[significant_neurons_df['Signi'].isin(['Y', 'N'])]
