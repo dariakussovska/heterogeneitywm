dceb09afc8870c4bde5cec8bc0eee8a3d554c9b7
Daria Kussovska <159807265+dariakussovska@users.noreply.github.com>
2025-10-21 13:57:20 -0400

    Update file paths for data and output

--- 01_task/main_1d.py
+++ 01_task/main_1d.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-reaction_data = pd.read_excel('/home/daria/PROJECT/graph_data/graph_probe.xlsx')
+reaction_data = pd.read_excel('/./graph_data/graph_probe.xlsx')
 reaction_data['response_time'] = reaction_data['Probe_End_Time'] - reaction_data['Probe_Start_Time']
 df_correct = reaction_data[reaction_data['response_accuracy'] == 1]
 subject_medians = df_correct.groupby(['subject_id', 'num_images_presented'])['response_time'].median().reset_index()
@@ -18,5 +18,5 @@ plt.title("Reaction Time Across Subjects and Memory Loads")
 plt.xticks(ticks=[0, 1, 2], labels=[1, 2, 3])
 plt.grid(True, linestyle="--", alpha=0.4)
 plt.tight_layout()
-plt.savefig("/home/daria/PROJECT/01_task/response_time_boxes.eps", format="eps", dpi=300)
+plt.savefig("/./01_task/response_time_boxes.eps", format="eps", dpi=300)
 plt.show()
