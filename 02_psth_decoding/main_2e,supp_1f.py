3efae2971f7d0d1ea5887f3ca378f5ab1f49e42c
Daria Kussovska <159807265+dariakussovska@users.noreply.github.com>
2025-10-22 13:52:59 -0400

    Update main_2e,supp_1f.py

--- 02_psth_decoding/main_2e,supp_1f.py
+++ 02_psth_decoding/main_2e,supp_1f.py
@@ -8,7 +8,7 @@ from statsmodels.stats.multitest import multipletests
 # Load data
 df_delay = pd.read_excel('../graph_data/graph_delay.xlsx')
 df_fixation = pd.read_excel('../clean_data/cleaned_Fixation.xlsx')
-df_regions = pd.read_excel('../all_neuron_brain_regions_cleaned.xlsx')
+df_regions = pd.read_excel('../data/all_neuron_brain_regions_cleaned.xlsx')
 trial_info = pd.read_excel('../trial_info.xlsx')
 
 region_groups = {
