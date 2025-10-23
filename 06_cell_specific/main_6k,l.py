ebf1f12989f662a5ddc641ed4b2b7f7bf6a2bc47
Daria Kussovska <159807265+dariakussovska@users.noreply.github.com>
2025-10-23 15:23:36 -0400

    Update main_6k,l.py

--- 06_cell_specific/main_6k,l.py
+++ 06_cell_specific/main_6k,l.py
@@ -9,6 +9,7 @@ from scipy.ndimage import label, find_objects
 import ast
 
 trial_info = pd.read_excel('../data/new_trial_final.xlsx')
+df_clustering = pd.read_excel('../Clustering_3D.xlsx')
 subject_trials = trial_info[trial_info['subject_id'] == 14][['trial_id_final', 'num_images_presented', 'stimulus_index_enc1', 'stimulus_index_enc2', 'stimulus_index_enc3', 'response_accuracy']]
 print(subject_trials)
 
