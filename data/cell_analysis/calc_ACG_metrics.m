18c8a09b2aeb2e3d6dad5e15e575edfed9d4f6e0
Daria Kussovska <159807265+dariakussovska@users.noreply.github.com>
2026-02-17 12:06:02 -0500

    Refactor calc_ACG_metrics for additional parameters
    Updated the calc_ACG_metrics function to include sample rate and optional parameters. Enhanced autocorrelogram calculations and added metrics visualization.

--- data/cell_analysis/calc_ACG_metrics.m
+++ data/cell_analysis/calc_ACG_metrics.m
@@ -1,88 +1,64 @@
-function acg_metrics = calc_ACG_metrics(spikes)
-    % Inputs:
-    %   spikes - structure containing 'times' (cell array of spike times) and 'total' (number of spikes)
-    
-    % Ensure that spikes.times is a cell array
-    if ~iscell(spikes.times)
-        error('spikes.times must be a cell array');
-    end
+function acg_metrics = calc_ACG_metrics(spikes,sr,varargin)
+% Two autocorrelograms are calculated:  narrow (100ms, 0.5ms bins) and wide (1s, 1ms bins) using the CCG function (for speed)
+%
+% Further three metrics are derived from these:
+%
+% Theta modulation index:
+%    Computed as the difference between the theta modulation trough (defined as mean of autocorrelogram bins 50-70 ms)
+%    and the theta modulation peak (mean of autocorrelogram  bins 100-140ms) over their sum
+%    Originally defined in Cacucci et al., JNeuro 2004
+%
+% BurstIndex_Doublets:
+%    max bin count from 2.5-8ms normalized by the average number of spikes in the 8-11.5ms bins
+%
+% BurstIndex_Royer2012:
+%    Burst index is determined by calculating the average number of spikes in the 3-5 ms bins of the spike
+%    autocorrelogram divided by the average number of spikes in the 200-300 ms bins.
+%    Metrics introduced in Royer et al. Nature Neuroscience 2012, and adjusted in Senzai & Buzsaki, Neuron 2017.
 
-    numcells = length(spikes.times);  % Number of neurons
+% By Peter Petersen
+% petersen.peter@gmail.com
+% Last edited: 06-10-2020
+p = inputParser;
+addParameter(p,'showFigures',true,@islogical);
+parse(p,varargin{:})
 
-    % Initialize metrics
-    ThetaModulationIndex = nan(1, numcells);
-    BurstIndex_Royer2012 = nan(1, numcells);
-    BurstIndex_Doublets = nan(1, numcells);
+spikes.numcells = length(spikes.times);
 
-    % Define bins for ACG calculation
-    bins_wide = 1001;   % Total 1001 bins for wide ACG (±500 ms, 1 ms bins)
-    bins_narrow = 201;  % Total 201 bins for narrow ACG (±50 ms, 0.5 ms bins)
+ThetaModulationIndex = nan(1,spikes.numcells);
+BurstIndex_Royer2012 = nan(1,spikes.numcells);
+BurstIndex_Doublets = nan(1,spikes.numcells);
 
-    % Initialize matrices for storing ACGs
-    acg_wide = zeros(bins_wide, numcells);     % 1001 bins
-    acg_narrow = zeros(bins_narrow, numcells); % 201 bins
+cell_indexes = find(spikes.total>0);
 
-    disp('Calculating narrow ACGs (±50 ms, 0.5 ms bins) and wide ACGs (±500 ms, 1 ms bins)');
-    tic
-    for i = 1:numcells
-        spike_times = spikes.times{i};  % Access spike times for this neuron
+bins_wide = 500;
+acg_wide = zeros(bins_wide*2+1,numel(spikes.times));
+bins_narrow = 100;
+acg_narrow = zeros(bins_narrow*2+1,numel(spikes.times));
+disp('Calculating narrow ACGs (100ms, 0.5ms bins) and wide ACGs (1s, 1ms bins)')
+tic
+for i = cell_indexes
+    acg_wide(:,i) = CCG(spikes.times{i},ones(size(spikes.times{i})),'binSize',0.001,'duration',1,'norm','rate','Fs',1);
+    acg_narrow(:,i) = CCG(spikes.times{i},ones(size(spikes.times{i})),'binSize',0.0005,'duration',0.100,'norm','rate','Fs',1);
+    % Metrics from narrow
+    BurstIndex_Doublets(i) = max(acg_narrow(bins_narrow+1+5:bins_narrow+1+16,i))/mean(acg_narrow(bins_narrow+1+16:bins_narrow+1+23,i));
+    % Metrics from wide
+    ThetaModulationIndex(i) = (mean(acg_wide(bins_wide+1+100:bins_wide+1+140,i)) - mean(acg_wide(bins_wide+1+50:bins_wide+1+70,i)))/(mean(acg_wide(bins_wide+1+50:bins_wide+1+70,i))+mean(acg_wide(bins_wide+1+100:bins_wide+1+140,i)));
+    BurstIndex_Royer2012(i) = mean(acg_wide(bins_wide+1+3:bins_wide+1+5,i))/mean(acg_wide(bins_wide+1+200:bins_wide+1+300,i));
+end
+toc
 
-        if length(spike_times) > 1
-            % Calculate narrow ACG (±50 ms, 0.5 ms bins)
-            acg_narrow(:, i) = autocorrelogram(spike_times, 0.0005, 0.05, bins_narrow);
-
-            % Calculate wide ACG (±500 ms, 1 ms bins)
-            acg_wide(:, i) = autocorrelogram(spike_times, 0.001, 0.5, bins_wide);
-
-            % Metrics from narrow ACG (BurstIndex_Doublets)
-            BurstIndex_Doublets(i) = max(acg_narrow(106:116, i)) / mean(acg_narrow(116:123, i));
-
-            % Metrics from wide ACG (ThetaModulationIndex)
-            ThetaModulationIndex(i) = (mean(acg_wide(601:640, i)) - mean(acg_wide(550:570, i))) / ...
-                                      (mean(acg_wide(550:570, i)) + mean(acg_wide(601:640, i)));
-
-            % Metrics from wide ACG (BurstIndex_Royer2012)
-            BurstIndex_Royer2012(i) = mean(acg_wide(503:505, i)) / mean(acg_wide(700:800, i));
-        else
-            % If less than 2 spikes, the metrics are NaN
-            acg_narrow(:, i) = NaN;
-            acg_wide(:, i) = NaN;
-            BurstIndex_Doublets(i) = NaN;
-            ThetaModulationIndex(i) = NaN;
-            BurstIndex_Royer2012(i) = NaN;
-        end
-    end
-    toc
-
-    % Store the results in a structure
-    acg_metrics.acg_wide = acg_wide;
-    acg_metrics.acg_narrow = acg_narrow;
-    acg_metrics.thetaModulationIndex = ThetaModulationIndex;
-    acg_metrics.burstIndex_Royer2012 = BurstIndex_Royer2012;
-    acg_metrics.burstIndex_Doublets = BurstIndex_Doublets;
+if p.Results.showFigures
+    figure, subplot(3,1,1)
+    histogram(ThetaModulationIndex,40),xlabel('Theta modulation index'), ylabel('Count')
+    subplot(3,1,2)
+    histogram(BurstIndex_Royer2012,40),xlabel('BurstIndex Royer2012'), ylabel('Count')
+    subplot(3,1,3)
+    histogram(BurstIndex_Doublets,40),xlabel('BurstIndex Doublets'), ylabel('Count')
 end
 
-function acg = autocorrelogram(spike_times, bin_size, duration, expected_num_bins)
-    % Initialize ACG
-    acg = zeros(expected_num_bins, 1);  % Initialize the ACG with correct size
-    
-    % Compute time differences between spikes (for autocorrelation)
-    for i = 1:length(spike_times)
-        diffs = spike_times - spike_times(i);
-        diffs(i) = [];  % Remove self-coincidence (lag = 0)
-        
-        % Convert differences to bins
-        bins = round(diffs / bin_size);
-        
-        % Keep only valid bins within the expected range
-        valid_bins = bins(abs(bins) <= floor(expected_num_bins / 2));  % Half for positive and half for negative lags
-        
-        % Increment the ACG at those valid bins
-        for bin = valid_bins'
-            acg(bin + floor(expected_num_bins / 2) + 1) = acg(bin + floor(expected_num_bins / 2) + 1) + 1;
-        end
-    end
-    
-    % Normalize by spike count and bin size
-    acg = acg / (length(spike_times) * bin_size);
-end
+acg_metrics.acg_wide = acg_wide;
+acg_metrics.acg_narrow = acg_narrow;
+acg_metrics.thetaModulationIndex = ThetaModulationIndex;
+acg_metrics.burstIndex_Royer2012 = BurstIndex_Royer2012;
+acg_metrics.burstIndex_Doublets = BurstIndex_Doublets;
